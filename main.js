0d842e63dfe16df2acdff2e0b7f01088c8a57e7f
S-Core21 <ogbonnashedrach8@gmail.com>
2023-03-04 10:43:11 +0100

    commit

--- main.js
+++ main.js
@@ -18,14 +18,17 @@ if (phoneSize1.matches) {
     aboutMeText.textContent = 'Eget ultrices vitae etiam nunc iaculis ipsum magna. Phasellus libero morbi tincidunt cras. Tincidunt dignissim euismod dui egestas donec proin est. Blandit amet erat viverra sit egestas. Ornare cursus sit aliquet ullamcorper pretium cras non. Pellentesque sit ut phasellus interdum molestie maecenas erat pretium, hendrerit. Et facilisi mauris'
     mentorWriteUp.textContent = 'get ultrices vitae etiam nunc iaculis ipsum magna. Phasellus libero morbi tincidunt cras. Tincidunt dignissim euismod dui egestas donec proin est.  Blandit amet erat viverra sit egestas.'
     NewsWriteUp.textContent = 'get ultrices vitae etiam nunc iaculis ipsum magna. Phasellus libero morbi tincidunt cras'
+    window.onscroll = function () {
+        document.querySelector('nav').style.cssText = 'box-shadow: -10px 0px 20px -5px #000086;'
+    }
 
 } else {
     console.log('firsts')
 }
 
-window.onscroll = function () {
-    document.querySelector('nav').style.cssText = 'box-shadow: -10px 0px 20px -5px #000086;'
-}
+// window.onscroll = function () {
+//     document.querySelector('nav').style.cssText = 'box-shadow: -10px 0px 20px -5px #000086;'
+// }
 
 
 
