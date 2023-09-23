5e03dbe6352f8e05ab8e657b74d41462eb9d0d52
S-Core21 <ogbonnashedrach8@gmail.com>
2023-09-23 15:38:25 +0100

    updates

--- newsletter_blog.js
+++ newsletter_blog.js
@@ -12,17 +12,18 @@ fetch('https://blogapi-31c0.onrender.com/blogs')
             const tagTwos = document.getElementById(`tagTwo-${index}`)
             const tagThrees = document.getElementById(`tagThree-${index}`)
             const links = document.getElementById(`link${index}`)
+            const imgs = document.getElementById(`img${index}`)
             let title = item.title
             const titleLInk = title.split(' ').join('-')
             links.addEventListener('click', () => {
                 window.location.href = `https://www.google.com/${titleLInk}`
             })
             headers.textContent = item.title
-            snippets.textContent = item.snippet.slice(0, 80)
+            snippets.textContent = item.snippet.slice(0, 80) + '...'
             tags.textContent = item.tagOne
             tagTwos.textContent = item.tagTwo
             tagThrees.textContent = item.tagThree
-            date.textContent = item.createdAt.slice(0, 10)
+            imgs.setAttribute('src', item.image)
         })
     })
     .catch(err => {
@@ -41,22 +42,35 @@ const data = {
     fullname: fullname.value,
     email: email.value
 }
-
+const regAlerrt = document.getElementById('regAlert')
+const regClose = document.getElementById('regClose')
 
 submit.addEventListener('click', () => {
     console.log(fullname.value, email.value)
     newsAlert.textContent = ''
     if (fullname.value.length > 2 && email.value.length > 2) {
-        fetch('https://blogapi-31c0.onrender.com/newsletter', {
+        fetch('https://blogapi-31c0.onrender.com/api/newsletter', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
+        .then(res=>{
+            console.log(res)
+            if(res.status === 200){
+                console.log('nice')
+                regAlerrt.style.cssText = 'display: block'
+            }
+        })
     }else{
         console.log('not valid')
         newsAlert.textContent = 'Add a valid Name or Email'
     }
 
+})
+
+
+regClose.addEventListener('click', ()=>{
+    regAlerrt.style.cssText = 'display: none'
 })
